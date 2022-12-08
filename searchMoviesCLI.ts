a02007e9403dba5ecbd771f6da3b134cd82dda78
Maria <mtens17@gmail.com>
2022-12-08 19:04:13 +0000

    Exercise completed

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -10,24 +10,34 @@ console.log("Welcome to search-movies-cli!")
 ;
 async function connect(){
     await client.connect()
-    // console.log("[1]Search [2]See your favourites [3]Quit")
-    // let option = question("Choose an action! [1,2,3]")
-    // if (option === '1'){
+//TODO: create a loop so that it asks every time done 
+    while(true){
+    console.log("[1]Search [2]See your favourites [3]Quit")
+    let option = question("Choose an action! [1,2,3]")
+    if (option === '1'){
         let searchTerm = question("Search term: ").toLowerCase()
-        const text = "select id, name, date, runtime, budget, revenue, vote_average, votes_count from movies where LOWER(name) like $1 order by date desc limit 10";
+        const searchQuery = "select id, name, date, runtime, budget, revenue, vote_average, votes_count from movies where LOWER(name) like $1 order by date desc limit 10";
         const values = [`%${searchTerm}%`];
-        const res = await client.query(text, values);
-        console.table(res.rows) // how to have them as list, and say 0 cancel 
-    //     let favChoice = question("Choose a movie row number to favourite [1...8/0]: ")
-    //     // insert the movie id to the favourites table 
-    //     console.log("Saving favourite movie: ") //how to get the name of the film here 
-    // }
-    // if (option === '2'){
-    //     console.log("Here are your saved favourites!")
-    //     const res = await client.query("select ");// need query that joins favourites to casts_view
-    // }
-    // console.log("All done!")
-    await client.end()
-}
-// }
+        const res = await client.query(searchQuery, values);
+        console.table(res.rows) 
+        // TODO: have them as list, and say 0 cancel 
+        let favChoice = question("Choose a movie row number to favourite [1...8/0]: ")
+        let favChoiceNum: number  = parseInt(favChoice)
+        const favMovieId = res.rows[favChoiceNum].id
+        const favAddQuery = "insert into favourites(movie_id) values ($1)"
+        const movieId = [`${favMovieId}`]
+        const result = await client.query(favAddQuery, movieId)
+        console.log("Saving favourite movie: ", res.rows[0].name) 
+    }
+    if (option === '2'){
+        console.log("Here are your saved favourites!")
+        const res = await client.query("select movies.id, name, date, runtime, budget, revenue, vote_average, votes_count from movies join favourites on movies.id = favourites.movie_id");
+        console.table(res.rows)
+    }
+    if (option === '3'){
+        console.log("All done!")
+        await client.end()
+        break
+    }
+}}
 connect()
