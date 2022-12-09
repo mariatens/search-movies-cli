0005c9b4b132d5094c6bd67460369b1e3bd67984
Maria <mtens17@gmail.com>
2022-12-09 12:05:53 +0000

    All functionality working

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -19,7 +19,6 @@ async function connect(){
         const values = [`%${searchTerm}%`];
         const res = await client.query(searchQuery, values);
         console.table(res.rows) 
-        // TODO: have them as list, and say 0 cancel 
         let i =0
         for (let item of res.rows){
              i += 1
@@ -28,18 +27,24 @@ async function connect(){
         console.log("[0] Cancel")
         let favChoice = question(`Choose a movie row number to favourite [1...${res.rows.length}/0]: `)
         if (favChoice === '0'){
-            console.log("All done!")
-            await client.end()
-            break
+            continue
         }
         else{
         let favChoiceNum: number  = parseInt(favChoice)-1
         const favMovieId = res.rows[favChoiceNum].id
+        const check = await client.query("select id from favourites where movie_id = $1", [favMovieId])
+        if (check.rowCount>0){
+            console.log(`You have already added ${res.rows[favChoiceNum].name} to your favourites list`)
+            continue
+        }
+        else{
         const favAddQuery = "insert into favourites(movie_id) values ($1)"
         const movieId = [`${favMovieId}`]
         const result = await client.query(favAddQuery, movieId)
-        console.log("Saving favourite movie: ", res.rows[0].name) 
-        }}
+        console.log("Saving favourite movie: ", res.rows[favChoiceNum].name) 
+        }
+        }
+    }
     if (option === '2'){
         console.log("Here are your saved favourites!")
         const res = await client.query("select movies.id, name, date, runtime, budget, revenue, vote_average, votes_count from movies join favourites on movies.id = favourites.movie_id");
